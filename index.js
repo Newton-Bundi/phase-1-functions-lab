5fcba23df3bf1813f06509c80a82af808878498f
Newton <newton.bundi@student.moringaschool.com>
2023-07-13 23:44:32 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+
+distanceFromHqInBlocks(30);
+distanceFromHqInFeet(30);
+
+function distanceFromHqInBlocks(passengerStreet){
+    return Math.abs(passengerStreet-42);
+}
+
+function distanceFromHqInFeet(passengerStreet){
+    return (Math.abs(passengerStreet-42)*264);
+}
+
+function distanceTravelledInFeet(destination,start){
+    return (Math.abs(destination-start) * 264);
+}
+
+function calculatesFarePrice(destination,start){
+    const distanceFeet = Math.abs(destination-start) * 264;
+    let fare
+
+    if (distanceFeet <= 400 ){
+        fare = 0;
+    } else if (distanceFeet> 400 && distanceFeet <=2000){
+        fare = (distanceFeet-400) * 2/100;
+    } else if (distanceFeet>2000 && distanceFeet <=2500){
+        fare = 25
+    }
+    else{
+        fare = 'cannot travel that far'
+    }
+
+    return fare;
+}
+
